757fbeb727e653d0e0fbd518f1e43fafde51f5a8
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-02-19 23:13:02 +0530

    design

---
+++ landingpage/src/App.js
@@ -0,0 +1,21 @@
+import Nav from "./components/Nav"
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <main className='max-w-[1920px] mx-auto bg-white overflow-hidden'>
+        <Hero/>
+        <div className="h-[3000px]"> </div>
+       </main >
+
+ 
+  )
+}
+
+export default App
+
+
+/***
+ * to-do1 : design the parent container
+ * to-do2: hero page design
+ * **/
---
+++ landingpage/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+
+    <section className='hero h-[640px] xl:h-[800px] bg-hero bg-center xl:bg-cover bg-fixed bg-no-repeat xl:rounded-bl-[290px]'>
+      <div className='container mx-auto'>
+        <div>Text</div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
